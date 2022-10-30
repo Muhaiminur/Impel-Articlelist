df85b255fa641a319fead3294ccf6de94057fd95
muhaiminurabir <muhaiminurabir@gmail.com>
2022-10-30 17:46:58 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Impel Article
+ 
---
+++ app/src/main/java/com/impel/impelArticle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.impel.impelArticle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
